bded08ef65a83726d47734a8ed489ecae2918b4e
João Victor <joaovictordasilva0911@gmail.com>
2026-03-02 21:08:03 -0300

    Setup Design System

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div>
-      <h2>Hello World</h2>
+      <Button>Click me</Button>
     </div>
   );
 }
